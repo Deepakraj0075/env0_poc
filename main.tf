e0eca78c23509a57c833ecec15ddfefee3fb7d29
aldeepakraj321 <aldeepakraj321@gmail.com>
2025-07-22 11:18:18 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+# ---------CREATING S3 DATA BUCKET  --------------
+resource "aws_s3_bucket" "thiss3" {
+  bucket = "${var.env}-${var.region}-${var.bucket_name}-logs"
+  tags = {
+    Name            = "${var.env}-${var.bucket_name}"
+    Environment     = var.env
+    Managed_by      = "Terraform"
+  }
+}
+
+# ------------------ RESTRICTING PUBLIC ACCESS FOR THE DATA BUCKET / versioning enabled--------------------
+resource "aws_s3_bucket_public_access_block" "block_public_access_data" {
+  bucket                  = aws_s3_bucket.thiss3.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_versioning" "s3_data_bucket_versioning" {
+  bucket = aws_s3_bucket.thiss3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ variables 2.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Enter bucket name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
